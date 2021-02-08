0794f19f944f83ff7409fc28572cb104f8102393
Daljit Singh <daljit.dokal@yahoo.co.nz>
2021-02-08 16:40:38 +1300

    Step 5 added to install gitlab via composer

---
+++ 5-install-gitlab-via-composer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# Next up we need to copy file to gloud vm
+
+
+# Retrieve the vm name
+export gcp_machine_name=$(gcloud compute instances list --limit=1 --format='value(name.basename())')
+
+# Copy file to vm
+gloud compute scp docker-compose.yaml $gcp_machine_name --ssh-key-file ~/.ssh/$USER --strict-host-key-checking=no
+
+# Install gotlan with docker compose
+sleep 2
+gcloud compute ssh $gcp_machine_name --ssh-key-file ~/.ssh/$USER -- 'sudo docker compose up -d'
