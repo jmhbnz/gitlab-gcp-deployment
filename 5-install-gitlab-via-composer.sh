17c6b3bccfd96ac95940491ba372418bb5ca0d5f
Daljit Singh <daljit.dokal@yahoo.co.nz>
2021-02-08 16:59:00 +1300

    Step 5 remote vm path updated

--- 5-install-gitlab-via-composer.sh
+++ 5-install-gitlab-via-composer.sh
@@ -8,8 +8,8 @@
 export gcp_machine_name=$(gcloud compute instances list --limit=1 --format='value(name.basename())')
 
 # Copy file to vm
-gloud compute scp docker-compose.yaml $gcp_machine_name --ssh-key-file ~/.ssh/$USER --strict-host-key-checking=no
+gcloud compute scp docker-compose.yaml $gcp_machine_name:/tmp --ssh-key-file ~/.ssh/$USER --strict-host-key-checking=no
 
 # Install gotlan with docker compose
-sleep 2
-gcloud compute ssh $gcp_machine_name --ssh-key-file ~/.ssh/$USER -- 'sudo docker compose up -d'
+#sleep 2
+#gcloud compute ssh $gcp_machine_name --ssh-key-file ~/.ssh/$USER -- 'sudo docker compose up -d'
