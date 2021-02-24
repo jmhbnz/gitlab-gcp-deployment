19001803aeaa09a60ff0b17daa516b51261e79fa
Daljit Singh <daljit.dokal@yahoo.co.nz>
2021-02-24 16:24:19 +1300

    Step script added to create secrets in gh.
    Co-authored-by: jmhbnz <mail@jamesblair.net>

--- 3-create-github-secrets.sh
+++ 3-create-github-secrets.sh
@@ -3,8 +3,9 @@
 
 # Once new service account is created and key.json file is created, We will have to upload these details into GitHub and automate the end to end CI/CD workflow.
 
-# Tange the shell block below to a shell script by pressing *, b t* in emacs command mode:
+# Tangle the shell block below to a shell script by pressing *, b t* in emacs command mode:
 
-# Once new srvice account is created and key.json file is created, We will have to upload these details into GitHub and automate the end to end CI/CD workflow.
 
-TODO
+echo $gcp_project_id | gh secret set GCP_PROJECT_ID
+cat key.json | jq '.client_email' -r | gh secret set GCP_EMAIL
+cat key.json | gh secret set GCP_CREDENTIALS
